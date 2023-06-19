204f69e271599ea35dba86c50e1d2d6e650e6702
“Ikenna <Ikenna.okpo@aurea.com>
2023-06-19 08:37:53 +0100

    fix custom button props

--- client/components/Button.tsx
+++ client/components/Button.tsx
@@ -1,15 +1,8 @@
 "use client";
 
-import { Button } from "@mui/material";
+import { Button, type ButtonProps } from "@mui/material";
 
-interface CustomButtonProps {
-    variant?: "text" | "outlined" | "contained";
-    color?: "primary" | "inherit" | "secondary" | "success" | "error" | "info" | "warning";
-    children: any;
-    onClick?: () => void;
-}
-
-const CustomButton = ({ variant, color, children, onClick}: CustomButtonProps) => {
+const CustomButton = ({ variant, color, children, onClick}: ButtonProps) => {
     return <Button onClick={onClick} variant={variant || 'contained'} color={color || 'primary'}>{children}</Button>
 }
 
